1244862fdc3577ca74b2f4d7207141d23b9e4229
Daniel Valcarce <davamix@gmail.com>
2019-04-03 08:09:24 +0200

    Added the info parameter to show basic information about the video

--- extractor.py
+++ extractor.py
@@ -1,3 +1,5 @@
+# coding=utf-8
+
 # https://stackoverflow.com/a/33399711/844372
 
 # python extractor.py video_file.mp4 --frames 10
@@ -5,22 +7,37 @@
 import cv2
 import argparse
 import os
+import sys
 from tqdm import tqdm
+
+# Return the total frames in the video
+def get_frames_video(video):
+    total_frames = int(video.get(cv2.CAP_PROP_FRAME_COUNT))
+    return total_frames
+
+# Show basic information about the video
+def show_info_video(video):
+    print("Frames: {}".format(get_frames_video(video)))
+    print("Frame rate: {}".format(video.get(cv2.CAP_PROP_FPS)))
+    print("Height: {}".format(video.get(cv2.CAP_PROP_FRAME_HEIGHT)))
+    print("Width: {}".format(video.get(cv2.CAP_PROP_FRAME_WIDTH)))
 
 # Arguments builder
 parser = argparse.ArgumentParser()
-parser.add_argument("file", help="Video file")
-parser.add_argument("--frames", help="Percentage of total frames to extract (int)", type=int)
-args = parser.parse_args()
+parser.add_argument('file', help='Video file')
+parser.add_argument('--frames', '-f', help='Percentage of total frames to extract (int)', type=int)
+parser.add_argument('--info', '-i', dest='info', action='store_true', help='Show information about the video (do not extract anything)')
+parser.set_defaults(info=False)
 
+args = parser.parse_args()
 video_file = args.file
 frames_to_extract = args.frames if args.frames else 0
 
 if not os.path.isfile(video_file):
     print("File '{}' does not exists".format(video_file))
-    exit()
+    sys.exit()
 
-# Open video    
+# Open the video    
 video = cv2.VideoCapture(video_file)
 
 # Read the 1st frame 
@@ -30,8 +47,12 @@ current_frame = 0
 total_frames_captured = 0
 
 if success:
-    # Get total frames in the video
-    total_frames = int(video.get(cv2.CAP_PROP_FRAME_COUNT))
+    if args.info:
+        show_info_video(video)
+        sys.exit()
+
+    # Get the total frames in the video
+    total_frames = get_frames_video(video)
     
     frames_to_skip = 0
     if frames_to_extract > 0:
